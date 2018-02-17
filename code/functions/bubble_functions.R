34df898ed8775afbd16ed3013596f0ab8a3a37b5
Nic Dalmasso <ndalmass@cmu.andrew.edu>
2018-02-17 14:31:01 -0500

    Committing new function for estimating errors around bubble CIs and wrapper

--- code/functions/bubble_functions.R
+++ code/functions/bubble_functions.R
@@ -97,7 +97,7 @@ create_CI_bubble_step_track <- function(df_points_step_track, center_idx, alpha_
   
   # In order to select the level, we select the closest (1-alpha)% of them
   ordered_path_prob_df <- data.frame(cbind(df_points_step_track, distance_vec))
-  ordered_path_prob_df <- ordered_path_prob_df[order(ordered_path_prob_df[, 3), ]
+  ordered_path_prob_df <- ordered_path_prob_df[order(ordered_path_prob_df[, 3]), ]
   ind_sel <- ceiling(n_df * (1-alpha_level))
   out_df <- ordered_path_prob_df[c(1:ind_sel), ]
   
@@ -139,7 +139,7 @@ bubbleCI <- function(dflist, center_idx, alpha_level = 0.1, long = 1, lat = 2, o
 }
 
 
-#' Estimating uncertainty around Bubble CIs
+#' Estimating uncertainty around Bubble CIs - NSWE Direction Approach
 #' 
 #' @description
 #' This function takes in a dataframe of points, which are representing the points inside a bubble
@@ -158,7 +158,7 @@ bubbleCI <- function(dflist, center_idx, alpha_level = 0.1, long = 1, lat = 2, o
 #' directions, the third one is a list the central point with the maximum distance of the CI bubble and
 #' the fourth one is the third lists reduced to a dataframe.
 #' 
-error_bands_bubbleCI <- function(bubble_ci_list, long = 1, lat = 2, distance = 3, 
+error_bands_bubbleCI_NSWE<- function(bubble_ci_list, long = 1, lat = 2, distance = 3, 
                                      conversion_unit_distance = 'm') {
 
   error_bands_NS <- list()
@@ -194,7 +194,7 @@ error_bands_bubbleCI <- function(bubble_ci_list, long = 1, lat = 2, distance = 3
   for (i in c(1:length(center_radius_list))) {
     center_radius_df = c(center_radius_df, center_radius_list[[i]][c(1,2,3)])
   }
-  center_radius_df = data.frame(t(matrix(center_radius_df, nrow=3))
+  center_radius_df = data.frame(t(matrix(center_radius_df, nrow=3)))
   
   return(list('errors_NS' = error_bands_NS, 'errors_EW' = error_bands_EW,
                'center_points_list_distance' = center_radius_list,
@@ -202,7 +202,99 @@ error_bands_bubbleCI <- function(bubble_ci_list, long = 1, lat = 2, distance = 3
 }
 
 
-#' Check if points are inside CI bubble
+#' Estimating uncertainty of bubble CI
+#' 
+#' @description
+#' This function takes in a list of dataframes, representing the different CI bubbles
+#' built on the track of the most "central" points, and extract center points, radiuses and
+#' extreme of the CI bubbles per each step
+#' 
+#' @param bubble_steps_CI list of dataframes, with each dataframe representing one step in 
+#' the track of the most central TC
+#' @param long_col Column index of the longitude
+#' @param lat_col Column index of the latitude
+#' @param unit_measure Unit measure of the distances 
+#' 
+#' @output List with centers, radius, positives and negatives dataframes
+error_bands_bubbleCI <- function(bubble_steps_CI, long_col = 1, lat_col = 2, 
+                                         unit_measure = "nautical mile") {
+
+  # Calculating centers and radius
+  centers <- sapply(bubble_steps_CI,function(df) df[1,]) %>% 
+                        t() %>%
+                        data.frame() %>% 
+                        select(long,lat) %>%
+                        sapply(function(x) matrix(x, ncol = 1))
+
+  centers <- data.frame(long = as.numeric(centers[, long_col]),
+                        lat = as.numeric(centers[, lat_col])) # dataframe in list column problem
+  radius <- sapply(bubble_steps_CI,function(df) max(df$distance_vec))
+  
+  # Calculating positive and negative diretions
+  out_mat <- distAlongP(data.df = centers)
+  bearing <- out_mat[[2]]
+  bearing_orthog <- bearing + 90
+  
+  positive <- matrix(0,nrow = nrow(centers), ncol = 2 )
+  negative <- matrix(0,nrow = nrow(centers), ncol = 2 )
+  
+  for (i in 1:nrow(centers)) {
+    radius_ind <- uconv(radius[i], from = unit_measure, to = "m", "Length")
+    direction <- bearing_orthog[i]
+    positive[i,] <- destPoint(centers[i,], direction, radius_ind)
+    negative[i,] <- destPoint(centers[i,], direction, -radius_ind)
+  }
+  
+  positive <- positive[-nrow(positive),]
+  negative <- negative[-nrow(negative),]
+  
+  return(list(centers = centers,
+              radius = radius,
+              positive = positive,
+              negative = negative)) 
+}
+
+
+#' Estimating Area of the Bubble CI
+#' 
+#' @description
+#' Finds area of bubble CI, for each step, then takes the maximum
+#' 
+#' @details
+#' This functions uses as inputs the output of error_bands_bubbleCI_posneg.
+#' 
+#' @param tc_bubble_structure Bubble CI list with centers, radius, positive 
+#' and negative parts
+#' 
+#' @output List with the area of the Bubble CI for each step and the 
+#' maximum of such values.
+#' 
+max_cumulative_area <- function(tc_bubble_structure) {
+
+  # Checking whether the negative attributes are in the correct format
+  # Positive are assumed to be the same as negative
+
+  if (!(is.data.frame(tc_bubble_structure$negative)) & !(is.matrix(tc_bubble_structure$negative))){
+    return(list(area_vec = 0, max = 0))
+  }
+  
+  neg <- tc_bubble_structure$negative
+  pos <- tc_bubble_structure$positive
+  n = nrow(neg)
+  neg_back <- neg[n:1,]
+  
+  area_vec <- c()
+  for (i in 2:nrow(neg)) {
+    dat_all <- rbind(pos[1:i,],
+                     neg_back[(n - i):n,])
+    spPoly <- SpatialPolygons(list(Polygons(list(Polygon(dat_all)),ID = 1)))
+    area_vec <- c(area_vec, gArea(spPoly))
+  }  
+  return(list(area_vec = area_vec, max = max(area_vec)))
+}
+
+
+#' Check if points are inside bubble CI
 #' 
 #' @description
 #' Given a dataframe of points this function returns a vector of the same
@@ -210,15 +302,15 @@ error_bands_bubbleCI <- function(bubble_ci_list, long = 1, lat = 2, distance = 3
 #' bubble.
 #' 
 #' @param df_points Dataframe of points which inclusion needs to be calculated
-#' @param center_radius_df Dataframe of central points with associated radius of the CI
-#' bubble
+#' @param center_df Dataframe of central points of the CI bubble
+#' @param radius_df Dataframe of radius of the CI bubble
 #' @param long column index of the longitude in the central points
 #' @param lat column index of the latitude in the central points
-#' @param radius column index of the radius in the central points
 #' 
 #' @output Boolean vectors on whether the points are in any of the bubble CIs or not
-check_points_in_bubbleCI <- function(df_points, center_radius_df, long = 1, lat = 2, radius =3){
+check_points_in_bubbleCI <- function(df_points, center_df, radius_df, long = 1, lat = 2){
 
+  center_radius_df <- cbind(center_df[, c(long,lat)], radius_df)
   n_df <- dim(df_points)[1]
   in_vec <- numeric(n_df)
   
@@ -235,3 +327,31 @@ check_points_in_bubbleCI <- function(df_points, center_radius_df, long = 1, lat 
   }  
   return(in_vec)
 }
+
+
+#' Creating bubble CI out of TC simulated lists
+#' 
+#' @description
+#' This function is a wrapper to determine CI bubble for list of simulated TCs
+#' 
+#' @param dflist List of simulated TCs
+#' @param center_idx Index of the path that needs to be considered as a center
+#' @param alpha_level Alpha level of the bubble CI
+#' @param long Column index of the longitude
+#' @param lat Column index of the latitude
+#' @param unit_measure Unit of measure used for distance
+bubble_ci_from_tclist <- function(dflist, center_idx, alpha_level = 0.1, 
+                                 long = 1, lat = 2, unit_measure = 'nautical mile') {
+
+    bubble_steps_CI <- bubbleCI(dflist = dflist, center_idx = center_idx, alpha_level = alpha_level, 
+                                long = long, lat = lat, output_length = unit_measure)
+
+    bubble_ci_structure <- error_bands_bubbleCI(bubble_steps_CI = bubble_steps_CI, 
+                                                long_col = long, lat_col = lat, unit_measure = unit_measure)
+
+    area_list <- max_cumulative_area(bubble_ci_structure)
+
+    return(list('bubble_CI_object' = bubble_ci_structure, 'area' = area_list[[2]], 'area_vector' = area[[1]]))
+} 
+
+
