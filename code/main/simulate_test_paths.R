a6eba25e437a146f3640017869f99cce585034dd
Robin Dunn <robin.dunn1012@gmail.com>
2018-02-20 13:35:56 -0500

    Update to simulate 350 TCs per specification

--- code/main/simulate_test_paths.R
+++ code/main/simulate_test_paths.R
@@ -11,6 +11,6 @@ proc.time() - ptm
 
 # Save environment w/ one list per test TC.
 # Each list has length 4 (one for each auto/non-auto death_reg/no_death_reg combo)
-# Each of those 4 lists have length 1000 (all simulations from given starting observations)
+# Each of those 4 lists have length 350 (all simulations from given starting observations)
 test_env <- list2env(test_sims)
-save(test_env, file = "data/generate/Test_Sims_1000.Rdata")
+save(test_env, file = "data/generate/Test_Sims_350.Rdata")
