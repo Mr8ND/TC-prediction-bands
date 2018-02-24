27e1df7ab768489e2b4feeb8ac8aae0ee3e93a17
Nic Dalmasso <ndalmass@cmu.andrew.edu>
2018-02-24 13:05:08 -0500

    Removing old code files and old code chunks

--- code/functions/path_functions.R
+++ code/functions/path_functions.R
@@ -101,89 +101,6 @@ distBetweenP <- function(data_df_p1, data_df_p2,
 }
 
 
-#' #' calculates the distance matrix Delta between n paths
-#' #'
-#' #' TODO: I'm confused why the diagonal should be / is 1? - Nic? 
-#' #' when I run this (in your credible_interval_pipeline) I get 0.
-#' #' 
-#' #' TODO: this function is outdated. Should be removed.
-#' #'
-#' #' @param path_mat_list 
-#' #' @param output_length 
-#' #' @param longlat 
-#' #'
-#' #' @return
-#' #' @export
-#' #'
-#' #' @examples
-#' distMatrixPath <- function(path_mat_list, output_length = "nautical mile", 
-#'                            longlat = TRUE){
-#' 	#This function calculates the distance matrix Delta between n paths.
-#' 	#This matrix is a symmetric matrix, with the diagonal being 1, and each of the off
-#' 	#diagonal element is the sum of the absolute distance of each point of the two paths.
-#' 
-#' 	#Input is a list of path. Each path is a matrix of (k x 2) elements, in which each
-#' 	#row is a pair of longitude latitude points.
-#' 	#Default is that the pair is (longitude, latitude), which is the flag variable
-#' 	#"longlat". If this is set to false then both the dataframes will be simply swapped.
-#' 
-#' 	#Ouput is the delta matrix distance, output as a matrix (n x n).
-#' 
-#' 	n_mat <- length(path_mat_list)
-#' 	output_mat <- matrix(, nrow = n_mat, ncol = n_mat)
-#' 
-#' 	for (i in c(1:n_mat)) {
-#' 		for (j in c(i:n_mat)) {
-#' 			output_mat[i,j] <- sum(distBetweenP(path_mat_list[[i]], 
-#' 			                                   path_mat_list[[j]],
-#' 			                                   longlat = longlat))
-#' 			output_mat[j,i] <- output_mat[i,j]
-#' 		}
-#' 	}
-#' 
-#' 	return(output_mat)
-#' }
-# 
-# distMatrixPath_train_test = function(path_mat_list_train,path_mat_list_test, output_length="nautical mile", longlat= TRUE){
-#   #This function calculates the distance matrix Delta between n paths.
-#   #This matrix is a symmetric matrix, with the diagonal being 1, and each of the off
-#   #diagonal element is the sum of the absolute distance of each point of the two paths.
-#   
-#   #Input is a list of path for training data and a list of path for test data. 
-#   # Each path is a matrix of (k x 2) elements, in which each
-#   #row is a pair of longitude latitude points.
-#   #Default is that the pair is (longitude, latitude), which is the flag variable
-#   #"longlat". If this is set to false then both the dataframes will be simply swapped.
-#   # Need both groups to be in the same order
-#   
-#   #Ouput is the delta matrix distance, output as a matrix (m x n). m test,n training paths
-#   
-#   n_mat = length(path_mat_list_test)
-#   m_mat = length(path_mat_list_train)
-#   output_mat = matrix(, nrow=n_mat, ncol=m_mat)
-#   
-#   for (i in c(1:n_mat)){
-#     for (j in c(1:m_mat)){
-#       output_mat[i,j] = sum(distBetweenP(path_mat_list_test[[i]], path_mat_list_train[[j]],longlat=longlat))
-#     }
-#   }
-#   
-#   return(output_mat)
-# }
-
-# 
-# probMatrixPath = function(dist_matrix, epsilon = 430){
-# 	#Given a certain distance Matrix D, this function gives back the probability matrix
-# 	#indicated in the paper, by first trasforming it using the exp(-D/epsilon).
-# 	#Epsilon is by default 430, but it can be changed using the related variable.
-# 
-# 	#The input is a matrix (n x n).
-# 	#Ouput is a matrix (n x n), with the rows summing to one.
-# 
-# 	w = exp(-1*(dist_matrix/epsilon))
-# 	return(w/rowSums(w))
-# }
-
 # Extensions to probMatrix Creation --------------------------------
 
 #' Finds the kth smallest distance for each point
@@ -230,43 +147,6 @@ probMatrixPath_k = function(dist_matrix, kNN_sigma, kNN_sigma_new = kNN_sigma){
   return(p)
 }
 
-
-#' 
-#' #' Given a certain probability matrix P, this function computes the matrix 
-#' #' D^2(x,y) defines as follows:
-#' #'   D^2(x,y) = sum_k (P(x,k)-P(y,k))/stat_k
-#' #' Where stat_k is the stationary vector of the probability matrix.
-#' #'
-#' #' NOTE: This function always assumes that the probability matrix is built 
-#' #' correctly and that all the rows sums to one, such that an eigenvalue 1 always
-#' #' exists - and it is the biggest of the series. So the function goes and looks
-#' #' for the first eigenvector of the series.
-#' #' 
-#' #'
-#' #' @param prob_matrix probability/transition matrix (n x n)
-#' #' @param t power to raise the transition
-#' #'
-#' #' @return D^2(x,y) matrix (n x n)
-#' #' @export
-#' #'
-#' #' @examples
-#' d2MatrixPath <- function(prob_matrix, t = 5){
-#' 	prob_mat_t <- prob_matrix**t
-#' 	eigenvec_1 <- eigen(prob_matrix)$vectors[,1]
-#' 
-#' 	n_mat <- dim(prob_mat_t)[1]
-#' 	output_mat <- matrix(, nrow = n_mat, ncol = n_mat)
-#' 
-#' 	for (i in c(1:n_mat)) {
-#' 		for (j in c(i:n_mat)) {
-#' 			d2_vec <- (prob_matrix[i,] - prob_matrix[j,])/(eigenvec_1**(1/2))
-#' 			output_mat[i,j] <- as.numeric(d2_vec %*% d2_vec)
-#' 			output_mat[j,i] <- output_mat[i ,j] 
-#' 		}
-#' 	}
-#' 
-#' 	return(output_mat)
-#' }
 
 # Extensions to d2MatrixPath Creation -----------------------------
 
@@ -471,33 +351,6 @@ distMatrixPath_innersq = function(path_mat_list,
   return(output_mat)
 }
 
-# distMatrixSpeed_innersq <- function(speed_df){
-#   # this function is similar to the ones above - but we get the sum of square 
-#   # differences of the speed (this ends up being just a wrapper)
-#   #
-#   #
-# 
-#   distance = dist(speed_df)
-#   d2 = as.matrix(distance)^2
-# 
-#   return(d2)
-# }
-
-# Train to Test (t2t) ---------------------------------------------
-
-#   #This function calculates the distance matrix Delta between n paths.
-#   #This matrix is a symmetric matrix, with the diagonal being 1, and each of the off
-#   #diagonal element is the sum of the absolute distance of each point of the two paths.
-#   
-#   #Input is a list of path for training data and a list of path for test data. 
-#   # Each path is a matrix of (k x 2) elements, in which each
-#   #row is a pair of longitude latitude points.
-#   #Default is that the pair is (longitude, latitude), which is the flag variable
-#   #"longlat". If this is set to false then both the dataframes will be simply swapped.
-#   # Need both groups to be in the same order
-#   
-#   #Ouput is the delta matrix distance, output as a matrix (m x n). m test,n training paths
-#   
 
 #' Create path distance matrix
 #' 
@@ -547,27 +400,3 @@ distMatrixPath_t2t_path <- function(path_mat_list_train, path_mat_list_test,
   
   return(output_mat)
 }
-
-
-
-
-
-
-# 
-# distMatrixPath_t2t_speed = function(speed_mat_list_train,speed_mat_list_test){
-#   # creates a speed distance matrix between the test paths and training paths
-#   # contains the correct L2 norm structure sum of square distances (obv)
-# 
-#   mat_train = t(sapply(speed_mat_list_train,function(x) x[[1]]))
-#   mat_test  = t(sapply(speed_mat_list_test,function(x) x[[1]]))
-# 
-#   n_test  = nrow(mat_test)
-#   n_train = nrow(mat_train)
-#   
-#   both = rbind(mat_train,mat_test)
-#   all_dist = as.matrix(dist(both))
-# 
-#   output_mat = all_dist[(n_train+1):(n_test+n_train),1:n_train]
-# 
-#   return(output_mat)
-# }
