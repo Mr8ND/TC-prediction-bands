f26c686221f6d03a3d84c93eab2d4cfd979df5f0
Nic Dalmasso <ndalmass@cmu.andrew.edu>
2018-02-27 19:44:17 -0500

    Committing main code to run the credible interval pipeline from terminal. Rscript credible_interval_pipeline start_idx end_idx. Indeces are defaulted to 0 and the length of the test_env which is imported

--- main/credible_interval_pipeline.R
+++ main/credible_interval_pipeline.R
@@ -1,83 +1,68 @@
 #' Library ----------------------------------------
-
-#library(datamart)
-#library(geosphere)
-#library(plyr)
-#library(rworldmap)
-#library(caret)
-#library(ks)
-#library(gtools)
-#library(RANN)
-
-
-#' Sourcing external functions ----------------------------------------
-
-#functions_loc <- '../TCcrediblebands/R/'
-#source(paste0(functions_loc, 'kde_functions.R'))
-#source(paste0(functions_loc, 'bubble_functions.R'))
-#source(paste0(functions_loc, 'convex_hull.R'))
-#source(paste0(functions_loc, 'delta_ball.R'))
-#source(paste0(functions_loc, 'thirteen_point_compression.R'))
-#source(paste0(functions_loc, 'path_functions.R'))
-#source(paste0(functions_loc, 'depth_function.R'))
-#source(paste0(functions_loc, 'credible_interval_pipeline_functions.R'))
-
-# Install from Github ----------------------------------
-
 library(devtools)
+library(methods)
+
+#' Install from Github ----------------------------------
+
 devtools::install_github(repo = 'Mr8ND/Hurricanes_701/TCcrediblebands')
 library(TCcrediblebands)
 
+#' Loading all the data --------------------------------------
+
+true_curve_conversion_function <- function(df){
+  output_df = data.frame(cbind(df$long, df$lat))
+  names(output_df) = c('long', 'lat')
+  return(output_df)
+}
+
+data_loc <- "main/data/"
+load(paste0(data_loc, 'Test_Sims_350.Rdata'))
+load(paste0(data_loc, 'train_test_names.Rdata'))
+load(paste0(data_loc, 'raw_data.Rdata'))
+
+true_curve_test <- lapply(test_data, true_curve_conversion_function)
+
+
+#' Getting the initial arguments -----------------------------
+
+start_idx_passed <- 1
+end_idx_passed <- length(test_env)
+n_sim_curve_total <- 25
+alpha_level <- 0.1
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) > 0) {
+  start_idx_passed <- args[1]
+  end_idx_passed <- args[2]
+}
+
 #' Execution --------------------------------------------------------
 
-load('data/sample/Test_Sims_1000.Rdata')
+desired_curves_types <- c("Auto_DeathRegs","Auto_NoDeathRegs",
+                          "NoAuto_DeathRegs", "NoAuto_NoDeathRegs")
 
-#complexity_mat_results <- matrix(rep(NA,16), ncol=2, nrow=8)
-#num_samples_vec <- c(50, 100, 150, 250, 350, 500, 750, 1000)
-
-
-#for (idx_sim in c(1:8)) {
-reduced_test_env <- list("AL031951" = list("Auto_DeathRegs"= list(),
-                                           "Auto_NoDeathRegs" = list(),
-                                           "NoAuto_DeathRegs" = list(),
-                                           "NoAuto_NoDeathRegs" = list()),
-                         "AL011951" = list("Auto_DeathRegs" = list(),
-                                           "Auto_NoDeathRegs" = list(),
-                                           "NoAuto_DeathRegs" = list(),
-                                           "NoAuto_NoDeathRegs" = list()))
-
-for (name_tc in names(reduced_test_env)) {
-  for (curve_type in names(reduced_test_env[[name_tc]])) {
-    for (i in c(1:10)) { # or num_samples_vec[idx_sim] if we are testing for complexity
-      reduced_test_env[[name_tc]][[curve_type]][[i]] <- test_env[[name_tc]][[curve_type]][[i]]
+tc_full_sim_list <- list()
+tc_true_path_list <- list()
+for (name_tc in names(test_env)) {
+  for (curve_type in desired_curves_types) {
+    for (i in c(1:n_sim_curve_total)) { 
+      tc_full_sim_list[[name_tc]][[curve_type]][[i]] <- 
+                                          test_env[[name_tc]][[curve_type]][[i]]
+      tc_true_path_list[[name_tc]] <- true_curve_test[[name_tc]]
     }
   }
 }
 
-
-# Data import step as dflist - list of lists of TCs
-tc_full_sim_list <- reduced_test_env
-
-# Data import step for true TCs DF - test ones
-tc_true_path_list <- list("AL031951" = test_env$AL031951$Auto_DeathRegs[[1]],
-                          "AL011951" = test_env$AL011951$Auto_DeathRegs[[1]])
-alpha_level <- 0.1
-
-start.time <- Sys.time()
 output_pipeline <- credible_interval_pipeline(tc_full_sim_list = tc_full_sim_list,
-                                                tc_true_path_list = tc_true_path_list,
-                                                alpha_level = alpha_level)
-end.time <- Sys.time()
-time.taken <- end.time - start.time
+                                          tc_true_path_list = tc_true_path_list,
+                                          alpha_level = alpha_level,
+                                          start_idx = start_idx_passed,
+                                          end_idx = end_idx_passed)
 
-print(c(idx_sim, as.numeric(time.taken)))
-
-complexity_mat_results[idx_sim,] <- c(num_samples_vec[idx_sim], as.numeric(time.taken)/2)
-#}
-
-#save(complexity_mat_results, file = 'data/complexit_mat_results.Rdata')
-
-
-out_filename <- paste0('data/output_pipeline_alpha', as.character(alpha_level), '_', Sys.Date(), '.Rdata')
+out_filename <- paste0(data_loc,
+                       'output_pipeline_alphalevel',
+                       as.character(alpha_level), '_',
+                       start_idx_passed, '_',
+                       end_idx_passed, '_',
+                       Sys.Date(), '.Rdata')
 save(output_pipeline, file = out_filename)
-
