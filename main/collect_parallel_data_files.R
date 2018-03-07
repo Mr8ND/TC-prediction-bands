43dcbcbef003e3b6797ee0cdf3c9a6c3c49d644f
benjaminleroy <benjaminleroyusa@gmail.com>
2018-03-07 15:32:42 -0500

    updates to visualization and some underlying kde assumptions

--- main/collect_parallel_data_files.R
+++ main/collect_parallel_data_files.R
@@ -6,30 +6,31 @@ library(tidyverse)
 data_loc <- "main/data/"
 
 list_of_files <- list.files(data_loc)[
-			list.files(data_loc) %>% grep(pattern = "sca_projection_info")
-									]
+            list.files(data_loc) %>% grep(pattern = "sca_projection_info")
+                                    ]
 # initialize names --------
 load(paste0(data_loc,'/Test_Sims_350.Rdata')) # for order
 
 all_projections <- list()
 for (tc_name in names(test_env)) {
-	all_projections[[tc_name]] <- 0
+    all_projections[[tc_name]] <- 0
 }
 
 # fill in all_projections ------
 
 for (fname in list_of_files) {
-	load(paste0(data_loc,fname))
-	for (tc_name in names(projection_out)) {
-		all_projections[[tc_name]] <- projection_out[[tc_name]]
-	}
+    load(paste0(data_loc,fname))
+    for (tc_name in names(projection_out)) {
+        all_projections[[tc_name]] <- projection_out[[tc_name]]
+    }
 }
 
 if ((sapply(all_projections,class) %>%
     table %>%
     length) == 1 & 
-      class(all_projections[[0]]) == "list") {
-  save(all_projections, file = "all_sca_projection_test.Rdata")
+      class(all_projections[[1]]) == "list") {
+  save(all_projections, file = paste0(data_loc,
+                                      "sca_projection_info_all.Rdata"))
 }
 
 
