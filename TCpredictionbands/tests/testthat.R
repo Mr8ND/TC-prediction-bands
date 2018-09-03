b110726ff2ded5ad843a5198d2f03e31729a678c
Nic Dalmasso <ndalmass@cmu.andrew.edu>
2018-09-03 17:59:48 -0400

    Adding subfolder structure for automatic testing within the package and modifying gitignore to include the checks and the tar.gz file which is generated when running R CMD check and R CMD build respectively

---
+++ TCpredictionbands/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(TCpredictionbands)
+
+test_check("TCpredictionbands")
---
+++ TCpredictionbands/tests/testthat/test-examples.R
@@ -0,0 +1,13 @@
+library(stringr)
+context("examples")
+test_that("str_length is number of characters", {
+  expect_equal(str_length("a"), 1)
+  expect_equal(str_length("ab"), 2)
+  expect_equal(str_length("abc"), 3)
+})
+
+test_that("str_length of factor is length of level", {
+  expect_equal(str_length(factor("a")), 1)
+  expect_equal(str_length(factor("ab")), 2)
+  expect_equal(str_length(factor("abc")), 3)
+})
